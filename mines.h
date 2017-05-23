9078fd4322535048e8d8af99435a51d52821f3d8
hitech95 <nicveronese@gmail.com>
2017-05-23 21:52:49 +0200

    Added Header for Mine

---
+++ mines.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2017 by Nicolò Veronese.
+// All Rights Reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <assert.h>
+
+#ifndef _MINE_H_
+#define _MINE_H_
+
+typedef struct MineCell_t *MineNode;
+typedef struct MineStruct_t *Mine;
+
+/*
+* Init the matrix on dynamic mem, using specified sizes.
+*/
+Mine Mine_init(int size);
+
+/**
+* Destroy the matrix, free the memory.
+**/
+void Mine_destroy(Mine game);
+
+/*
+ * Clear the matrix. Re-Init the game
+ */
+void Mine_clear(Mine game);
+
+/**
+* Process the current matriz, calculate cell danger
+**/
+void Mine_process(Mine game);
+
+/*
+* Return matrix size.
+*/
+int Mine_get_size(Mine game);
+
+int Mine_get_bomb_count(Mine game);
+
+int Mine_get(Mine game, int x, int y, MineNode * data);
+
+int Mine_pick(Mine game, int x, int y, int marked);
+
+int Mine_add_bomb(Mine game, int x, int y);
+
+int Mine_snapshot(Mine game);
+
+int Mine_rollback(Mine game);
+
+#endif

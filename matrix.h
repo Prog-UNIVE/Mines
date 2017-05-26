0fa4fabc220d4809ff617a017e110aad51fc0c87
Nicolò Veronese <nicveronese@gmail.com>
2017-05-26 16:06:16 +0200

    CRLF to LF

--- matrix.h
+++ matrix.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 by Nicol� Veronese.
+// Copyright (c) 2017 by Nicolò Veronese.
 // All Rights Reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy

f18e7a41131f281d01e998daa9aa6dce9a4e6e4a
hitech95 <nicveronese@gmail.com>
2017-05-28 15:26:32 +0200

    Mine: Fixes game implementation

--- mines.c
+++ mines.c
@@ -1,3 +1,26 @@
+/**
+* Copyright (c) 2017 by Nicolò Veronese.
+* All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+**/
+
 #include "mines.h"
 #include "stack.h"
 #include "queue.h"
@@ -19,15 +42,18 @@ struct MineStruct_t
 	int                 bombs;
 	int                 flags;
 	int                 shown;
+	int                 init;
 	Matrix				matrix;
 	Queue				fill_queue;
-	Stack				snapshot_stack;
+	Stack				snapshot_stack_matrix;
 };
 
-int Mine_flood_fill(Mine game,	MineNode *node);
+int Mine_flood_fill(Mine game, MineNode node);
+MineNode Mine_generate_node();
 int random_int(int min, int max);
 
-Mine Mine_init(int width, int height, int bombs) {
+Mine Mine_init(int width, int height)
+{
 	Mine game;
 
 	if ((game = (Mine)malloc(sizeof(struct MineStruct_t))) == NULL)
@@ -35,201 +61,390 @@ Mine Mine_init(int width, int height, int bombs) {
 
 	game->width = width;
 	game->height = height;
-	game->bombs = bombs;
+	game->bombs = 0;
 	game->flags = 0;
 	game->shown = 0;
+	game->init = 0;
 	game->matrix = Matrix_init(width, height);
 	game->fill_queue = QUEUE_init();
-	game->snapshot_stack = STACK_init();
+	game->snapshot_stack_matrix = STACK_init();
 
-	if (game->matrix == NULL || game->fill_queue == NULL || game->snapshot_stack == NULL)
+	if (game->matrix == NULL || game->fill_queue == NULL || game->snapshot_stack_matrix == NULL)
 		return NULL;
 
 	return game;
 }
 
-void Mine_destroy(Mine game) {
-	Matrix *ptr;
+void Mine_destroy(Mine game)
+{
+	Matrix matrix;
+
 	Matrix_destroy(game->matrix);
 	QUEUE_destroy(game->fill_queue);
 
-	while (STACK_size(game->snapshot_stack) > 0) {
-		STACK_pop(game->snapshot_stack, (void **)&ptr);
-		Matrix_destroy(*ptr);
+	while (STACK_size(game->snapshot_stack_matrix) > 0)
+	{
+		STACK_pop(game->snapshot_stack_matrix, (void **)&matrix);
+		Matrix_destroy(matrix);
 	}
-	STACK_destroy(game->snapshot_stack);
+	STACK_destroy(game->snapshot_stack_matrix);
 
 	free(game);
 }
 
-void Mine_clear(Mine game) {
-	Matrix *ptr;
+void Mine_clear(Mine game)
+{
+	int i, j;
+	MineNode node;
+	Matrix matrix;
 
 	game->flags = 0;
 	game->shown = 0;
-
-	Matrix_clear(game->matrix);
-	QUEUE_destroy(game->snapshot_stack);
+	
+	QUEUE_destroy(game->fill_queue);
 	game->fill_queue = QUEUE_init();
 
-	while (STACK_size(game->snapshot_stack) > 0) {
-		STACK_pop(game->snapshot_stack, (void **)&ptr);
-		Matrix_destroy(*ptr);
+	while (STACK_size(game->snapshot_stack_matrix) > 0)
+	{
+		STACK_pop(game->snapshot_stack_matrix, (void **)&matrix);
+		Matrix_destroy(matrix);
+	}	
+
+	for (i = 0; i < game->height; i++)
+	{
+		for (j = 0; j < game->width; j++)
+		{
+			Matrix_get(game->matrix, j, i, (void **)&node);
+			node->shown = MARK_HIDDEN;
+		}
 	}
 }
 
-void Mine_process(Mine game, int generate) {
+int Mine_process(Mine game, int generate)
+{
 	int i, j, k, weight;
-	MineNode *node;
+	MineNode node, node_s;
+	
+	if (game->init)
+	{
+		return CODE_ERROR;
+	}
 
-	if (generate) {
-		for (i = 0; i < game->bombs;) {
+	if (generate > 0)
+	{
+		for (i = 0; i < generate;)
+		{
 			j = random_int(0, game->width);
 			k = random_int(0, game->height);
 
-			if (!Mine_add_bomb(game, j, k))
+			if (Mine_add_bomb(game, j, k) == CODE_OK)
 				i++;
 		}
 	}
 
-	for (i = 0; i < game->width; i++) {
-		for (j = 0; j < game->height; j++) {
+	for (i = 0; i < game->height; i++)
+	{
+		for (j = 0; j < game->width; j++) 
+		{
 
-			Matrix_get(game->matrix, i, j, (void **)&node);
+			Matrix_get(game->matrix, j, i, (void **) &node);
 
-			if (node == NULL && (node = (MineNode *)malloc(sizeof(struct MineCell_t))) == NULL) {
-				printf("Error on malloc cells\n");
+			if (node == NULL && (node = Mine_generate_node()) == NULL)
+			{
+				printf("Error on malloc Mine node\n");
 				return;
-			}
+			}			
 
-			// TODO - Calculate weight
 			weight = 0;
+			if (!node->bomb)
+			{
+				for (k = j - 1; k <= j + 1; k++)
+				{
+					if (Matrix_get(game->matrix, k, i - 1, (void **)&node_s) == CODE_OK && node_s != NULL && node_s->bomb) 
+					{
+						weight++;
+					}
+					
+					if (Matrix_get(game->matrix, k, i + 1, (void **)&node_s) == CODE_OK && node_s != NULL && node_s->bomb)
+					{
+						weight++;
+					}
+				}
 
-			(*node)->x = i;
-			(*node)->y = j;
-			(*node)->weight = weight;
-			(*node)->shown = MARK_HIDDEN;
+				if (Matrix_get(game->matrix, j - 1, i, (void **)&node_s) == CODE_OK && node_s != NULL && node_s->bomb)
+				{
+					weight++;
+				}
 
-			Matrix_set(game->matrix, i, j, node);
+				if (Matrix_get(game->matrix, j + 1, i, (void **)&node_s) == CODE_OK && node_s != NULL && node_s->bomb)
+				{
+					weight++;
+				}
+			}
+			else
+			{
+				weight = 9;
+			}			
+
+			node->x = j;
+			node->y = i;
+			node->weight = weight;
+			node->shown = MARK_HIDDEN;
+
+			Matrix_set(game->matrix, j, i, node);
 		}
 	}
+
+	game->init = 1;
 }
 
-int Mine_get_width(Mine game) {
+int Mine_get_width(Mine game)
+{
 	return game->width;
 }
 
-int Mine_get_height(Mine game) {
+int Mine_get_height(Mine game) 
+{
 	return game->height;
 }
 
-int Mine_get_bomb_count(Mine game) {
+int Mine_get_bomb_count(Mine game)
+{
 	return game->bombs;
 }
 
-int Mine_get(Mine game, int x, int y, MineNode *data) {
-	return Matrix_get(game->matrix, x, y, (void **) &data);
+int Mine_get(Mine game, int x, int y, MineNode *data) 
+{
+	if (!game->init)
+	{
+		return CODE_ERROR;
+	}
+	return Matrix_get(game->matrix, x, y, (void **)data);
 }
 
-int Mine_pick(Mine game, int x, int y, int marker) {
-	MineNode *node;
+int Mine_pick(Mine game, int x, int y, int marker)
+{
+	MineNode node;
 
-	if (Matrix_get(game->matrix, x, y, (void **)&node) != CODE_OK) {
+	if (!game->init)
+	{
 		return CODE_ERROR;
 	}
 
-	Mine_snapshot(game);
+	if (Matrix_get(game->matrix, x, y, (void **) &node) != CODE_OK)
+	{
+		return CODE_ERROR;
+	}
 
-	if (marker == MARK_FLAG) {
-		if ((*node)->shown == MARK_FLAG) {
-			(*node)->shown = MARK_HIDDEN;
-			game->flags--;
+	if (marker == MARK_FLAG)
+	{
+		if (node->shown == MARK_FLAG)
+		{
+			node->shown = MARK_HIDDEN;
+			game->flags -= 1;
 		}
-		else if (game->flags < game->bombs) {
-			(*node)->shown = MARK_FLAG;
-			game->flags++;
+		else if (game->flags < game->bombs)
+		{
+			node->shown = MARK_FLAG;
+			game->flags += 1;
 		}
-		else {
+		else 
+		{
 			return CODE_INVALID;
 		}
 	}
-	else if (marker == MARK_SHOW) {
-		if ((*node)->bomb) {
-			return CODE_LOSE;
-		}
+	else if (marker == MARK_SHOW && node->shown == MARK_HIDDEN)
+	{	
+		Mine_snapshot(game);
 		Mine_flood_fill(game, node);
 
-		if (game->shown == (game->width * game->height) - game->bombs) {
-			return CODE_WIN;
-		}
+		if (node->bomb)
+		{
+			return CODE_LOSE;
+		}		
+	}
+
+	if (game->shown == (game->width * game->height) - game->bombs)
+	{
+		return CODE_WIN;
 	}
 
 	return CODE_OK;
 }
 
 int Mine_add_bomb(Mine game, int x, int y) {
-	MineNode *node;
+	MineNode node = NULL;
 
-	Matrix_get(game->matrix, x, y, (void **)&node);
-
-	if (node == NULL && (node = (MineNode *)malloc(sizeof(struct MineCell_t))) == NULL) {
+	if (game->init)
+	{
 		return CODE_ERROR;
 	}
 
-	if (!(*node)->bomb) {
-		(*node)->bomb = 1;
-		return Matrix_set(game->matrix, x, y, node);
+	if (Matrix_get(game->matrix, x, y, (void **)&node) == CODE_ERROR)
+	{
+		return CODE_ERROR;
 	}
 
-	return CODE_INVALID;
+ 	if (node == NULL && (node = Mine_generate_node()) == NULL)
+	{
+			return CODE_ERROR;
+	}
+
+	if (!node->bomb)
+	{
+		node->bomb = 1;
+		game->bombs += 1;
+	}
+	else
+	{
+		return CODE_INVALID;
+	}
+
+	return Matrix_set(game->matrix, x, y, node);
 }
 
-int Mine_snapshot(Mine game) {
-	// TODO - Do memory copy of this
-	return STACK_push(game->snapshot_stack, &game->matrix);
+int Mine_snapshot(Mine game)
+{
+	int i, j, *k;
+	MineNode node;
+	Matrix matrix;
+
+	if (!game->init) 
+	{
+		return CODE_ERROR;
+	}
+
+	if ((matrix = Matrix_init(game->width, game->height)) == NULL) 
+	{
+		return CODE_ERROR;
+	}
+
+	for (i = 0; i < game->height; i++)
+	{
+		for (j = 0; j < game->width; j++) 
+		{
+			
+			if (Matrix_get(game->matrix, j, i, (void **)&node) == CODE_OK &&
+				(k = (int *)malloc(sizeof(int))) != NULL) 
+			{
+				*k = node->shown;
+				Matrix_set(matrix, j, i, k);
+			}
+		}
+	}
+	
+	return STACK_push(game->snapshot_stack_matrix, matrix);;
 }
 
-int Mine_rollback(Mine game) {
-	Matrix *ptr;
-	if (STACK_pop(game->snapshot_stack, (void **)&ptr) == CODE_OK) {
-		game->matrix = *ptr;
+int Mine_rollback(Mine game)
+{
+	int i, j, *k, shown;
+	MineNode node;
+	Matrix matrix;
+
+	if (!game->init) 
+	{
+		return CODE_ERROR;
+	}
+
+	if (STACK_is_empty(game->snapshot_stack_matrix))
+	{
+		return CODE_INVALID;
+	}
+	
+	if (STACK_pop(game->snapshot_stack_matrix, (void **)&matrix) == CODE_OK)
+	{
+		shown = 0;
+		for (i = 0; i < game->height; i++)
+		{
+			for (j = 0; j < game->width; j++)
+			{				
+				if (Matrix_get(game->matrix, j, i, (void **)&node) == CODE_OK &&
+					Matrix_get(matrix, j, i, (void **)&k) == CODE_OK)
+				{
+					if (node->shown == MARK_SHOW)
+					{
+						node->shown = *k;
+
+						if (node->shown == MARK_SHOW)
+						{
+							shown++;
+						}
+					}
+				}
+				else
+				{
+					return CODE_ERROR;
+				}
+			}
+		}
+
+		Matrix_destroy(matrix);
+		game->shown = shown;
+
+		printf("Shown count: %d\n", shown);
+
 		return CODE_OK;
 	}
 
 	return CODE_ERROR;
 }
 
-int Mine_flood_fill(Mine game, MineNode *first_node) {
-	int x, y;
-	MineNode *node;
+int Mine_flood_fill(Mine game, MineNode first_node)
+{
+	int x, y, k;
+	MineNode node;
 
 	if (QUEUE_enqueue(game->fill_queue, first_node) == CODE_OK) {
 		while (QUEUE_size(game->fill_queue) > 0) {
-			if (QUEUE_dequeue(game->fill_queue, (void **)&node) != CODE_OK) {
+			if (QUEUE_dequeue(game->fill_queue, (void **)&node) != CODE_OK)
+			{
 				return CODE_ERROR;
 			}
 
-			(*node)->shown = MARK_SHOW;
-			game->shown++;
+			if (node->shown == MARK_HIDDEN) {
+				node->shown = MARK_SHOW;
+				game->shown++;
 
-			if ((*node)->weight == 0) {
-				x = (*node)->x;
-				y = (*node)->y;
+				if (node->weight == 0) {
+					x = node->x;
+					y = node->y;
 
-				if (Matrix_get(game->matrix, x, y - 1, (void **)&node) == CODE_OK) {
-					QUEUE_enqueue(game->fill_queue, node);
-				}
+					for (k = x - 1; k <= x + 1; k++) {
+						if (Matrix_get(game->matrix, k, y - 1, (void **)&node) == CODE_OK && node != NULL) {
+							QUEUE_enqueue(game->fill_queue, node);
+						}
 
-				if (Matrix_get(game->matrix, x, y + 1, (void **)&node) == CODE_OK) {
-					QUEUE_enqueue(game->fill_queue, node);
-				}
+						if (Matrix_get(game->matrix, k, y + 1, (void **)&node) == CODE_OK && node != NULL) {
+							QUEUE_enqueue(game->fill_queue, node);
+						}
+					}
 
-				if (Matrix_get(game->matrix, x - 1, y, (void **)&node) == CODE_OK) {
-					QUEUE_enqueue(game->fill_queue, node);
-				}
+					if (Matrix_get(game->matrix, x - 1, y, (void **)&node) == CODE_OK && node != NULL) {
+						QUEUE_enqueue(game->fill_queue, node);
+					}
 
-				if (Matrix_get(game->matrix, x + 1, y, (void **)&node) == CODE_OK) {
-					QUEUE_enqueue(game->fill_queue, node);
+					if (Matrix_get(game->matrix, x + 1, y, (void **)&node) == CODE_OK && node != NULL) {
+						QUEUE_enqueue(game->fill_queue, node);
+					}
+
+
+
+					/*if (Matrix_get(game->matrix, x, y - 1, (void **)&node) == CODE_OK) {
+						QUEUE_enqueue(game->fill_queue, node);
+					}
+
+					if (Matrix_get(game->matrix, x, y + 1, (void **)&node) == CODE_OK) {
+						QUEUE_enqueue(game->fill_queue, node);
+					}
+
+					if (Matrix_get(game->matrix, x - 1, y, (void **)&node) == CODE_OK) {
+						QUEUE_enqueue(game->fill_queue, node);
+					}
+
+					if (Matrix_get(game->matrix, x + 1, y, (void **)&node) == CODE_OK) {
+						QUEUE_enqueue(game->fill_queue, node);
+					}*/
 				}
 			}
 		}
@@ -238,6 +453,40 @@ int Mine_flood_fill(Mine game, MineNode *first_node) {
 	}
 
 	return CODE_ERROR;
+}
+
+MineNode Mine_generate_node()
+{
+	MineNode node;
+
+	if ((node = (MineNode)malloc(sizeof(struct MineCell_t))) == NULL) 
+	{
+		return NULL;
+	}
+
+	node->x = -1;
+	node->y = -1;
+
+	node->bomb = 0;
+	node->weight = 0;
+	node->shown = MARK_HIDDEN;
+
+	return node;
+}
+
+int Mine_is_node_bomb(MineNode node) 
+{
+	return node->bomb;
+}
+
+int Mine_is_node_shown(MineNode node)
+{
+	return node->shown;
+}
+
+int Mine_is_node_weight(MineNode node)
+{
+	return node->weight;
 }
 
 int random_int(int min, int max)
--- mines.h
+++ mines.h
@@ -1,29 +1,35 @@
-// Copyright (c) 2017 by Nicol� Veronese.
-// All Rights Reserved.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+/**
+* Copyright (c) 2017 by Nicolò Veronese.
+* All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+**/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
 #include <assert.h>
+
+#include "queue.h"
+#include "stack.h"
+#include "matrix.h"
 
 #ifndef _MINE_H_
 #define _MINE_H_
@@ -52,48 +58,170 @@
 #define MARK_SHOW 1
 #define MARK_FLAG 2
 
-
 typedef struct MineCell_t *MineNode;
 typedef struct MineStruct_t *Mine;
 
-/*
-* Init the matrix on dynamic mem, using specified sizes.
-*/
-Mine Mine_init(int width, int height, int bombs);
+/* INTERFACE FUNCTION DECLARATIONS */
+/**
+* Initializate the game.
+*
+* @return A reference - to a new game - if dynamic memory
+* allocation for the game was successful - or NULL otherwise. Take really
+* good care of this return value, since it will be needed as a parameter in
+* subsequent calls - to the majority of other game handling functions
+* in this game function interface - i.e. a sort of "handle" to the game.
+* @see Matrix_destroy()
+**/
+Mine Mine_init(int width, int height);
 
 /**
-* Destroy the matrix, free the memory.
+* Destroy the game.
+*
+* The fame is destroyed - that is, all memory occupied by the
+* matrix, the queue and the stack is deallocated.
+* When all ADTs and data have been deallocated - the
+* game header is deallocated, too.
+*
+* @param[in] game - a reference to current game.
+* @return Nothing.
+* @see Matrix_init()
 **/
 void Mine_destroy(Mine game);
 
-/*
- * Clear the matrix. Re-Init the game
- */
+/**
+* Clear the matrix.
+*
+* The gamematrix is cleared, all nodes are set to MASK_HIDDEN.
+* The queue and the stack are cleared.
+*
+* @param[in] mat - a reference to current matrix.
+**/
 void Mine_clear(Mine game);
 
 /**
 * Process the current matrix, calculate cell danger
-* if generate is true the field is generated
+* if generate is true the bombs are added to the field
 **/
-void Mine_process(Mine game, int generate);
+int Mine_process(Mine game, int generate);
 
-/*
-* Return matrix width.
-*/
+/**
+* Get the game matrix width.
+*
+* @param[in] game - a reference to the current fame.
+*
+* @return The width, that is, the number of columns in the matrix.
+**/
 int Mine_get_width(Mine game);
 
+/**
+* Get the game matrix height.
+*
+* @param[in] game - a reference to the current game.
+*
+* @return The height, that is, the number of rows in the matrix.
+**/
 int Mine_get_height(Mine game);
 
+/**
+* Get the bomb count in the game.
+*
+* @param[in] game - a reference to the current game.
+*
+* @return The bomb count in the matrix.
+**/
 int Mine_get_bomb_count(Mine game);
 
+/**
+* Get the node at the specified location.
+*
+* When called, the 4th parameter of this function, @a data,
+* should reference an (external, user-defined) pointer.
+* After the call - this referenced, external pointer has been
+* redirected, to point to the data of the element -
+* if the call was succesful.
+*
+* @param[in] mat - reference to current matrix.
+* @param[in] x - x coord
+* @param[in] y - y coord
+* @param[out] data - reference to a pointer. After the call,
+* this referenced pointer has been redirected to point to
+* the data of the element - if the call was successful.
+*
+* @return Value 0 - if the call was OK - or
+* value -1 otherwise.
+**/
 int Mine_get(Mine game, int x, int y, MineNode *data);
 
+/**
+* Add a bomb at location (x, y) of the current game.
+*
+* @param[in] game - a reference to the current game.
+* @param[in] x - the x coord.
+* @param[in] y - the y coord.
+* @param[in] marker - the operation code.
+*
+* @return Value 0 - if the call was OK - or
+* value -1 otherwise.
+**/
 int Mine_pick(Mine game, int x, int y, int marker);
 
+/**
+* Add a bomb at location (x, y) of the current game.
+*
+* @param[in] game - a reference to the current game.
+* @param[in] x - the x coord.
+* @param[in] y - the y coord.
+*
+* @return Value 0 - if the call was OK - or
+* value -1 otherwise.
+**/
 int Mine_add_bomb(Mine game, int x, int y);
 
+/**
+* Take a snapshot of the current game
+*
+* @param[in] game - a reference to the current game.
+*
+* @return Value 0 - if the call was OK - or
+* value -1 otherwise.
+**/
 int Mine_snapshot(Mine game);
 
+/**
+* Rollback to a previous snapshot.
+*
+* @param[in] game - a reference to the current game.
+*
+* @return Value 0 - if the call was OK - or
+* value -1 otherwise.
+**/
 int Mine_rollback(Mine game);
+
+/**
+* Get if node is a bomb.
+*
+* @param[in] node - a reference to the current node.
+*
+* @return If a node is a bomb.
+**/
+int Mine_is_node_bomb(MineNode node);
+
+/**
+* Get the node shown mask.
+*
+* @param[in] node - a reference to the current node.
+*
+* @return The show mask of a node.
+**/
+int Mine_is_node_shown(MineNode node);
+
+/**
+* Get the node weight.
+*
+* @param[in] node - a reference to the current node.
+*
+* @return The weight of a node.
+**/
+int Mine_is_node_weight(MineNode node);
 
 #endif
